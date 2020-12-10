5d06108d6ec81fde679a18ba6422ad66beb270fd
Hilwa Azzahra <hilwa.azzahra.corp@gmail.com>
2020-12-10 14:28:17 +0700

    first commit

---
+++ LaporanPenjualan.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LaporanPenjualan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CRV = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DTP1 = New System.Windows.Forms.DateTimePicker()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.DTP4 = New System.Windows.Forms.DateTimePicker()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.DTP3 = New System.Windows.Forms.DateTimePicker()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DTP2 = New System.Windows.Forms.DateTimePicker()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CRV
+        '
+        Me.CRV.ActiveViewIndex = -1
+        Me.CRV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CRV.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CRV.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.CRV.Location = New System.Drawing.Point(3, 126)
+        Me.CRV.Name = "CRV"
+        Me.CRV.Size = New System.Drawing.Size(1595, 872)
+        Me.CRV.TabIndex = 9
+        Me.CRV.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.DTP1)
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 21)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(209, 102)
+        Me.GroupBox2.TabIndex = 10
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Laporan Harian"
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button1.Location = New System.Drawing.Point(3, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(203, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "View"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DTP1
+        '
+        Me.DTP1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.DTP1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DTP1.Location = New System.Drawing.Point(3, 18)
+        Me.DTP1.Name = "DTP1"
+        Me.DTP1.Size = New System.Drawing.Size(203, 22)
+        Me.DTP1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.GroupBox4)
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.CRV)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1601, 1001)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(732, 30)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 90)
+        Me.Button5.TabIndex = 13
+        Me.Button5.Text = "GRAFIK2"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(651, 30)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 90)
+        Me.Button4.TabIndex = 2
+        Me.Button4.Text = "GRAFIK1"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Button3)
+        Me.GroupBox4.Controls.Add(Me.DTP4)
+        Me.GroupBox4.Location = New System.Drawing.Point(436, 21)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(209, 102)
+        Me.GroupBox4.TabIndex = 12
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Laporan Bulanan"
+        '
+        'Button3
+        '
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button3.Location = New System.Drawing.Point(3, 76)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(203, 23)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "View"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'DTP4
+        '
+        Me.DTP4.CustomFormat = " MMMM yyyy "
+        Me.DTP4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.DTP4.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DTP4.Location = New System.Drawing.Point(3, 18)
+        Me.DTP4.Name = "DTP4"
+        Me.DTP4.ShowUpDown = True
+        Me.DTP4.Size = New System.Drawing.Size(203, 22)
+        Me.DTP4.TabIndex = 0
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.DTP3)
+        Me.GroupBox3.Controls.Add(Me.Button2)
+        Me.GroupBox3.Controls.Add(Me.DTP2)
+        Me.GroupBox3.Location = New System.Drawing.Point(221, 21)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(209, 102)
+        Me.GroupBox3.TabIndex = 11
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Laporan Mingguan"
+        '
+        'DTP3
+        '
+        Me.DTP3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.DTP3.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DTP3.Location = New System.Drawing.Point(3, 40)
+        Me.DTP3.Name = "DTP3"
+        Me.DTP3.Size = New System.Drawing.Size(203, 22)
+        Me.DTP3.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button2.Location = New System.Drawing.Point(3, 76)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(203, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "View"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DTP2
+        '
+        Me.DTP2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.DTP2.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DTP2.Location = New System.Drawing.Point(3, 18)
+        Me.DTP2.Name = "DTP2"
+        Me.DTP2.Size = New System.Drawing.Size(203, 22)
+        Me.DTP2.TabIndex = 0
+        '
+        'LaporanPenjualan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1601, 1001)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "LaporanPenjualan"
+        Me.Text = "LaporanPenjualan"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CRV As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DTP1 As DateTimePicker
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DTP2 As DateTimePicker
+    Friend WithEvents DTP3 As DateTimePicker
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents DTP4 As DateTimePicker
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ LaporanPenjualan.vb
@@ -0,0 +1,51 @@
+﻿Public Class LaporanPenjualan
+
+    Private Sub LaporanPenjualan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DTP1.Format = DateTimePickerFormat.Custom
+        DTP1.CustomFormat = "yyyy-MM-dd"
+
+        DTP2.Format = DateTimePickerFormat.Custom
+        DTP2.CustomFormat = "yyyy-MM-dd"
+
+        DTP3.Format = DateTimePickerFormat.Custom
+        DTP3.CustomFormat = "yyyy-MM-dd"
+
+        DTP4.Format = DateTimePickerFormat.Custom
+        DTP4.CustomFormat = "MM-yyyy"
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        CRV.SelectionFormula = "({tabelpenjualan.tanggal}) = '" & DTP1.Text & "'"
+        CRV.ReportSource = "LaporanHarian.rpt"
+        CRV.RefreshReport()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CRV.SelectionFormula = "ToText{tabelpenjualan.tanggal} in Date ('" & DTP2.Text & "') to Date ('" & DTP3.Text & "')"
+        CRV.ReportSource = "LaporanMingguan.rpt"
+        CRV.RefreshReport()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        CRV.SelectionFormula = "month({tabelpenjualan.tanggal}) = (" & Month(DTP4.Text) & ") and year({tabelpenjualan.tanggal}) = (" & Year(DTP4.Text) & ")"
+        CRV.ReportSource = "LaporanBulanan.rpt"
+        CRV.RefreshReport()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        CRV.ReportSource = "Grafik1.rpt"
+        CRV.RefreshReport()
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        CRV.ReportSource = "Grafik2.rpt"
+        CRV.RefreshReport()
+
+    End Sub
+
+    Private Sub DTP4_ValueChanged(sender As Object, e As EventArgs) Handles DTP4.ValueChanged
+
+    End Sub
+End Class
